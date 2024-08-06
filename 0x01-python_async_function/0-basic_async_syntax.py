dc85ce97bedd554996896ea23ab2da89eb087515
Abraham Paul <codexapp44@gmail.com>
2024-08-06 01:29:41 +0100

    task 0 done

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,13 +3,17 @@
 a library to write concurrent code
 using the async/await syntax.
 """
+import asyncio
 import random
 
-async def wait_random(max_delay: int = 10):
+
+async def wait_random(max_delay: int = 10) -> float:
     """An asynchronous coroutine that takes in an integer
     argument (max_delay, with a default value of 10) named
     wait_random that waits for a random delay between 0 and
     max_delay (included and float value) seconds and
     eventually returns it.
     """
-    return random.uniform(0, max_delay)
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
